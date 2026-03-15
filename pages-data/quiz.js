0e140c664e03d99d5d84fd0da1ecd5a9c55c78d4
Artem Safin <artemsafin.webdev@gmail.com>
2026-03-15 17:10:01 +0300

    Первая страница квиза

---
+++ pages-data/quiz.js
@@ -0,0 +1,15 @@
+export default {
+  "/quiz-start.html": {
+    title: "Квиз про зубки",
+    quizPageTitle: "Квиз про зубки",
+    quizBackHref: "/index.html",
+    quizBackText: "Назад",
+    quizStartTitle: "12 простых вопросов",
+    quizStartDescription:
+      "Ответьте на вопросы на темы<br>изученных статей из прошлых уроков<br>и закрепите свои знания",
+    quizLeaderboardHref: "#",
+    quizLeaderboardText: "Таблица лидеров",
+    quizStartHref: "#",
+    quizStartButtonText: "Пройти квиз",
+  },
+};
