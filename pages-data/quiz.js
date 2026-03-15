df48b01499d2c90083ebb1b9631006c5978c84c5
Artem Safin <artemsafin.webdev@gmail.com>
2026-03-15 20:20:06 +0300

    Страница вопроса квиза

--- pages-data/quiz.js
+++ pages-data/quiz.js
@@ -9,7 +9,26 @@ export default {
       "Ответьте на вопросы на темы изученных статей из прошлых уроков и закрепите свои знания",
     quizLeaderboardHref: "#",
     quizLeaderboardText: "Таблица лидеров",
-    quizStartHref: "#",
+    quizStartHref: "/quiz.html",
     quizStartButtonText: "Пройти квиз",
+  },
+  "/quiz.html": {
+    title: "Квиз про зубки",
+    quizPageTitle: "Квиз про зубки",
+    quizBackHref: "/quiz-start.html",
+    quizBackText: "Назад",
+    quizCurrentQuestion: "1",
+    quizTotalQuestions: "12",
+    quizTimer: "1:44",
+    quizQuestionText:
+      "Какая концентрация фторидов (ppm) в зубной пасте считается оптимальной для подростков старше 12 лет?",
+    quizPrevHref: "/quiz-start.html",
+    quizNextHref: "#",
+    quizOptions: [
+      { value: "500", label: "500 ppm" },
+      { value: "1000", label: "1000 ppm" },
+      { value: "1450-1500", label: "1450–1500 ppm" },
+      { value: "2500", label: "2500 ppm" },
+    ],
   },
 };
