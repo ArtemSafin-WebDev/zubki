810bb1431d1b45208c66a327e70bf49360dc4ed0
Artem Safin <artemsafin.webdev@gmail.com>
2026-03-17 04:47:20 +0300

    По адаптиву профиля

--- src/js/ui/dashboard.ts
+++ src/js/ui/dashboard.ts
@@ -2,7 +2,7 @@ import Dashboard from "../classes/components/Dashboard";
 
 export default function dashboard() {
   const elements = Array.from(
-    document.querySelectorAll<HTMLElement>(".home__dashboard")
+    document.querySelectorAll<HTMLElement>(".home__dashboard, .profile-dashboard")
   );
 
   elements.forEach((element) => {
