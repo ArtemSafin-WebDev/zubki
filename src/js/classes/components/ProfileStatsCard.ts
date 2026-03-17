eb552f62a04c0ed22937297071c3aa4ebc288f15
Artem Safin <artemsafin.webdev@gmail.com>
2026-03-17 05:12:31 +0300

    По профилю

--- src/js/classes/components/ProfileStatsCard.ts
+++ src/js/classes/components/ProfileStatsCard.ts
@@ -1,6 +1,6 @@
 import Component from "../Component";
 import Swiper from "swiper";
-import { Navigation } from "swiper/modules";
+import { Navigation, Pagination } from "swiper/modules";
 
 class ProfileStatsCard extends Component {
   private swiperInstance: Swiper | null = null;
@@ -17,19 +17,28 @@ class ProfileStatsCard extends Component {
     const nextButton = this.element.querySelector<HTMLButtonElement>(
       ".profile-dashboard__stats-arrow--next",
     );
+    const pagination = this.element.querySelector<HTMLElement>(
+      ".profile-dashboard__stats-pagination",
+    );
 
-    if (!container || !prevButton || !nextButton) {
+    if (!container || !prevButton || !nextButton || !pagination) {
       throw new Error("Profile stats slider elements not found");
     }
 
     this.swiperInstance = new Swiper(container, {
-      modules: [Navigation],
+      modules: [Navigation, Pagination],
       slidesPerView: 1,
       slidesPerGroup: 1,
       speed: 260,
       centeredSlides: false,
       watchOverflow: true,
       allowTouchMove: true,
+      pagination: {
+        el: pagination,
+        clickable: true,
+        bulletClass: "profile-dashboard__stats-pagination-bullet",
+        bulletActiveClass: "profile-dashboard__stats-pagination-bullet--active",
+      },
       navigation: {
         prevEl: prevButton,
         nextEl: nextButton,
